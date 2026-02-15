a367c3d91f72d6f70194df4b3fa659283f0fb42e
Daniel Cruzado <danimalaga011@gmail.com>
2026-02-15 20:15:10 +0100

    feature: configure serve for production deployment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,7 @@
 export const environment = {
+  production: true,
   baseUrl: {
-    scheme: '',
-    ip: '',
+    scheme: 'https',
+    ip: 'scib-back-test-santander-production.up.railway.app', // Sin el https://, solo el dominio
   },
 };
